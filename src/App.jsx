904fdaec34fe6f3ff19cdb3e762c5fac5513219c
ElizabethMM36 <elizabethmathew10d@gmail.com>
2025-01-28 09:45:42 +0530

    intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((total, part) => total + part.exercises, 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part, index) => (
+      <Part key={index} part={part} />
+    ))}
+  </>
+);
+
 const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+
   return (
     <div>
-      <p>Hello world</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
